699055198c5d027e05c4af0aff13d2db412cffbf
makarim muhammad <jakamakarim@gmail.com>
2026-02-03 14:21:00 +0700

    feat: add profile data from localstorage

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -1,14 +1,29 @@
 import { Input } from "../component/input";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
+import { useState} from "react";
 function Profile() {
+    const [user, setUser] = useState(() => {
+    const userData = localStorage.getItem("user-data");
+    if (userData) {
+      const parsedData = JSON.parse(userData);
+      const activeUser = parsedData.find(user => user.isLoggedIn === true);
+      const { fullname, email, phone, address } = activeUser;
+      return { fullname, email, phone, address };
+    }
+    return {}; 
+  });
+
+  console.log("user", user);
+  console.log("name", user.fullname);
+  console.log("email", user.email);
   return (
     <div>
       <Header bgColor="bg-black" />
 
       <div className="flex flex-row pt-20 justify-center">
         <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-md min-w-[250px] flex-1">
-          <span>Ghaluh Wizard</span>
+          <span>{user.fullname || 'Guest'}</span>
           <button>Upload New Photo</button>
           <span>Since 20 January 2022</span>
         </div>
@@ -19,6 +34,7 @@ function Profile() {
               type="email"
               id="email"
               name="email"
+              value={user.fullname}
               placeholder="Enter Your Email"
               required
               iconAlt="Email Icon"
@@ -30,6 +46,7 @@ function Profile() {
               type="email"
               id="email"
               name="email"
+              value={user.email}
               placeholder="Enter Your Email"
               required
               iconAlt="Email Icon"
