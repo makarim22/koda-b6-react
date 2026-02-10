2fe0ebfff686f42129216deee3f1d3590071072b
makarim muhammad <jakamakarim@gmail.com>
2026-02-11 01:44:41 +0700

    feat: add react hook form on profile-page

--- my-app/src/pages/Profile.jsx
+++ my-app/src/pages/Profile.jsx
@@ -1,91 +1,84 @@
 import { Input } from "../component/input";
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
-import { useState} from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 function Profile() {
-    const [user, setUser] = useState(() => {
+  const [user, setUser] = useState(() => {
     const userData = localStorage.getItem("user-data");
     if (userData) {
       const parsedData = JSON.parse(userData);
-      const activeUser = parsedData.find(user => user.isLoggedIn === true);
+      const activeUser = parsedData.find((user) => user.isLoggedIn === true);
       const { fullname, email, phone, address } = activeUser;
       return { fullname, email, phone, address };
     }
-    return {}; 
+    return {};
   });
 
-  console.log("user", user);
-  console.log("name", user.fullname);
-  console.log("email", user.email);
+  const {
+    register,
+    handleSubmit,
+    // watch,
+    // formState: { errors },
+  } = useForm();
+
+  const users = JSON.parse(localStorage.getItem("user-data"));
+
+  const userLoggedIn = users.filter((user) => user.isLoggedIn === true);
+  console.log("log in usernya adalah", userLoggedIn);
+
+  const onSubmit = (data) => {
+    const users = JSON.parse(localStorage.getItem("user-data"));
+
+    const userLoggedIn = users.findIndex((user) => user.isLoggedIn === true);
+
+    const updatedUsers = [...users];
+
+    updatedUsers[userLoggedIn] = {
+      ...updatedUsers[userLoggedIn],
+      fullname: data.fullName,
+      email: data.email, 
+      phone: data.phone,
+      password: data.password,
+      address: data.address,
+    };
+
+     localStorage.setItem('user-data', JSON.stringify(updatedUsers));
+    console.log('Data pengguna berhasil diupdate:', updatedUsers[userLoggedIn]);
+
+    alert('Profil berhasil diupdate!');
+  };
+
   return (
     <div>
       <Header bgColor="bg-black" />
 
       <div className="flex flex-row pt-20 justify-center">
         <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-md min-w-[250px] flex-1">
-          <span>{user.fullname || 'Guest'}</span>
+          <span>{user.fullname || "Guest"}</span>
           <button>Upload New Photo</button>
           <span>Since 20 January 2022</span>
         </div>
         <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-md min-w-[250px] flex-1">
-          <div className="mb-2">
-            <Input
-              label="Full Name"
-              type="email"
-              id="email"
-              name="email"
-              value={user.fullname}
-              placeholder="Enter Your Email"
-              required
-              iconAlt="Email Icon"
-            />
-          </div>
-          <div className="mb-2">
-            <Input
-              label="Email"
-              type="email"
-              id="email"
-              name="email"
-              value={user.email}
-              placeholder="Enter Your Email"
-              required
-              iconAlt="Email Icon"
-            />
-          </div>
-          <div className="mb-2">
-            <Input
-              label="Phone"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter Your Email"
-              required
-              iconAlt="Email Icon"
-            />
-          </div>
-          <div className="mb-2">
-            <Input
-              label="Password"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter Your Email"
-              required
-              iconAlt="Email Icon"
-            />
-          </div>
-          <div className="mb-2">
-            <Input
-              label="Address"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter Your Email"
-              required
-              iconAlt="Email Icon"
-            />
-          </div>
-          <button>Submit</button>
+        
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <span>First Name</span>
+            <input label="fullname" {...register("fullName")} />
+            <span>Email</span>
+            <input {...register("email")} />
+            <span>Phone</span>
+            <input {...register("phone")} />
+            <span>Password</span>
+            <input {...register("password")} />
+            <span>Address</span>
+            <input {...register("address")} />
+
+            <input type="submit" />
+          </form>
         </div>
       </div>
 
