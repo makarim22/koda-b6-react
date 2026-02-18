6138a63bdf39e7edd3f5ee3210d3871e634bb05b
makarim muhammad <jakamakarim@gmail.com>
2026-02-18 17:04:58 +0700

    fix : typo in importing component

--- my-app/src/pages/RegisterPage.jsx
+++ my-app/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Input } from '../component/input';
+import { Input } from '../component/Input';
 import { Button } from '/src/component/Button';
 import AuthLayout from '/src/layouts/AuthLayout';
 import RegisterImg from '../assets/coffee-cup.svg';
