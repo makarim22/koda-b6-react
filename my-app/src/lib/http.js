311323181a9990aa04347fcc78652b074625d2ab
makarim muhammad <jakamakarim@gmail.com>
2026-03-17 13:35:00 +0700

    feat : add recommended-products endpoint

---
+++ my-app/src/lib/http.js
@@ -0,0 +1,16 @@
+
+const BASE_URL = import.meta.env.VITE_BASE_URL || "https://jaka-backend.camps.fahrul.id"
+
+function http(url, body ,opts={}){
+    const headers = {}
+    if(opts.token){
+        headers.Authorization = "Bearer "+opts.token
+    }
+    return fetch(BASE_URL + url, {
+        method: opts.method || "GET",
+        body,
+        headers
+    })
+}
+
+export default http;
--- my-app/src/pages/Homepage.jsx
+++ my-app/src/pages/Homepage.jsx
@@ -8,58 +8,73 @@ import Footer from "../layouts/Footer";
 import { Link } from "react-router-dom";
 import { ProductGrid } from "../component/ProductGrid";
 import TestimonyCard from "../component/TestimonyCard";
-import espresso from "../assets/icons/productPage/espresso.jfif"
-import latte from "../assets/icons/productPage/latte.jpg"
-import mocha from "../assets/icons/productPage/mocha.jfif"
-import americano from "../assets/icons/productPage/americano.jfif"
+// import espresso from "../assets/icons/productPage/espresso.jfif"
+// import latte from "../assets/icons/productPage/latte.jpg"
+// import mocha from "../assets/icons/productPage/mocha.jfif"
+// import americano from "../assets/icons/productPage/americano.jfif"
+import { useState, useEffect } from 'react' 
+import http from '../lib/http'
+
 
 const HomePage = () => {
-  const products = [
-    {
-      id: 1,
-      image: espresso,
-      title: "Espresso",
-      price: "IDR 15.000",
-      originalPrice: "IDR 18.000",
-      description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
-      rating: 5,
-      reviews: 0,
-      isFlashSale: true,
-    },
-    {
-      id: 2,
-      image: latte,
-      title: "Latte",
-      price: "IDR 19.000",
-      originalPrice: "IDR 22.000",
-      description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
-      rating: 5,
-      reviews: 0,
-      isFlashSale: true,
-    },
-    {
-      id: 3,
-      image: mocha,
-      title: "Mocha",
-      price: "IDR 21.000",
-      originalPrice: "IDR 24.000",
-      description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
-      rating: 4,
-      reviews: 0,
-      isFlashSale: true,
-    },
-    {
-      id: 4,
-      image: americano,
-      title: "Americano",
-      price: "IDR 12.000",
-      originalPrice: "IDR 15.000",
-      description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
-      rating: 4,
-      reviews: 0,
-      isFlashSale: false,
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 1,
+  //     image: espresso,
+  //     title: "Espresso",
+  //     price: "IDR 15.000",
+  //     originalPrice: "IDR 18.000",
+  //     description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
+  //     rating: 5,
+  //     reviews: 0,
+  //     isFlashSale: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     image: latte,
+  //     title: "Latte",
+  //     price: "IDR 19.000",
+  //     originalPrice: "IDR 22.000",
+  //     description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
+  //     rating: 5,
+  //     reviews: 0,
+  //     isFlashSale: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     image: mocha,
+  //     title: "Mocha",
+  //     price: "IDR 21.000",
+  //     originalPrice: "IDR 24.000",
+  //     description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
+  //     rating: 4,
+  //     reviews: 0,
+  //     isFlashSale: true,
+  //   },
+  //   {
+  //     id: 4,
+  //     image: americano,
+  //     title: "Americano",
+  //     price: "IDR 12.000",
+  //     originalPrice: "IDR 15.000",
+  //     description: 'You can explore the menu that we provide with fun and have their own taste and make your day better.',
+  //     rating: 4,
+  //     reviews: 0,
+  //     isFlashSale: false,
+  //   },
+  // ];
+
+  const [products, setProducts] = useState('');
+
+   const getProducts = async() => {
+    const req = await http("/admin/products/recommended-products")
+    const data = await req.json()
+    setProducts(data)
+    
+  }
+  useEffect(()=>{
+    getProducts()
+  },[])
 
   const testimonials = [
     {
